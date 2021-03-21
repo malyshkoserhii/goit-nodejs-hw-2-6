17df3b739463cf3fc962696771d3a10ad6d812f7
Malyshko Serhii <malyshkosergiy@gmail.com>
2021-03-21 07:12:51 +0200

    babel/se-lint parser; babel plugin private methods

---
+++ service/email.js
@@ -0,0 +1,14 @@
+const Mailgen = require('mailgen');
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
+class EmailService {
+    sender = sgMail;
+    Generatetemolate = Mailgen;
+    constructor(env) {
+    createTemplate(vereficationToken, name = 'Guest') {}        }
+    sendEmail()
+
+}
+
+module.exports = EmailService;
